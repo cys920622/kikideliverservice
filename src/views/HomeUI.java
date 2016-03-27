e1a554e738acf8ae4110fab677df5ef5a5af0aed
Daniel Choi <cys920622@gmail.com>
2016-03-27 02:25:57 -0700

    WIP - Add view-only, non-refreshing table to AddressUI. Fix missing breaks in HomeUI cases.

--- src/views/HomeUI.java
+++ src/views/HomeUI.java
@@ -47,11 +47,14 @@ public class HomeUI extends JPanel {
                     //should get back parcel info from pID
                     //should get back sender/receiver/status from dID through pID
                     //should probably be able to get back all the pID corresponding to dID
+                    f.add(new AddressUI());
                     f.add(new DeliveryUI());
                     f.add(new ParcelUI());
+                    break;
                 case "Clerk":
                     f.add(new DeliveryUI());
                     f.add(new ParcelUI());
+                    break;
                     //need to also add cash/credit info
 
                 case "Deliverer":
