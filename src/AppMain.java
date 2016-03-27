e573319d13b57108e060a82735e7b44487069f13
Daniel Choi <cys920622@gmail.com>
2016-03-27 00:01:58 -0700

    Fix typo in DB_URL, change UI to grid

--- src/AppMain.java
+++ src/AppMain.java
@@ -13,12 +13,13 @@ public class AppMain {
     public static void main(String[] args) {
         JFrame f = new JFrame();
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER));
+//        f.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER));
+        f.getContentPane().setLayout(new GridLayout(2,2));
         f.getContentPane().add(new AddressUI());
         f.getContentPane().add(new ParcelUI());
         f.getContentPane().add(new ClientsUI());
         f.getContentPane().add(new DeliveryUI());
-        f.setSize(600, 1300);
+        f.setSize(1200, 600);
         f.setVisible(true);
     }
 }
