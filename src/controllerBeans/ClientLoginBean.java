91be049592e8124e2e223c2b514b62473e4de803
Daniel Choi <cys920622@gmail.com>
2016-03-29 01:17:53 -0700

    Implement client login by client ID which queries all the deliveries for whom they are the sender or receiver. Results are shown only in print statements currently.

---
+++ src/controllerBeans/ClientLoginBean.java
@@ -0,0 +1,7 @@
+package controllerBeans;
+
+/**
+ * Created by danielchoi on 2016-03-28.
+ */
+public class ClientLoginBean {
+}
---
+++ src/views/ClientLoginUI.java
@@ -0,0 +1,7 @@
+package views;
+
+/**
+ * Created by danielchoi on 2016-03-28.
+ */
+public class ClientLoginUI {
+}
